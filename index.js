a43aad6987fa513d15effbba6197ba318323ae23
Francis Elmo Valeros <hed-fevaleros@smu.edu.ph>
2025-07-23 13:53:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.get('/supplies', (req, res) => {
     res.json(data);
 });
 
-const PORT = 3003;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
